bcffd4382cbc7af4e9ac4cd4a20db7ae84d3858a
kiryo06 <all.sentinel.0507@gmail.com>
2024-11-27 16:50:58 +0900

    up

--- GameSeisaku/FighterMorio/Map.cpp
+++ GameSeisaku/FighterMorio/Map.cpp
@@ -2,6 +2,7 @@
 #include "DxLib.h"
 #include "MapData.h"
 #include "Camera.h"
+#include <cassert>
 
 void Map::Init(int mapNumber)
 {
@@ -23,11 +24,11 @@ void Map::Init(int mapNumber)
 		break;
 	case 1:
 		// マップデータ読み込み
-		for (int hChip = 0; hChip < MapDataFile::kChipNumY; hChip++)
+		for (int hChip = 0; hChip < MapDataFile::k1ChipNumY; hChip++)
 		{
-			for (int wChip = 0; wChip < MapDataFile::kChipNumX; wChip++)
+			for (int wChip = 0; wChip < MapDataFile::k1ChipNumX; wChip++)
 			{
-				PrototypeChipData[hChip][wChip].chipKind = MapDataFile::mapChipData[hChip][wChip];
+				PrototypeChipData[hChip][wChip].chipKind = MapDataFile::mapChipData1[hChip][wChip];
 				PrototypeChipData[hChip][wChip].w = MapDataFile::kChipWidth;
 				PrototypeChipData[hChip][wChip].h = MapDataFile::kChipHeight;
 				PrototypeChipData[hChip][wChip].pos.x = wChip * MapDataFile::kChipWidth;
@@ -43,58 +44,44 @@ void Map::Init(int mapNumber)
 
 void Map::Draw(int mapNumber, Camera* camera)
 {
+	int MapChipNumY = 0;
+	int MapChipNumX = 0;
 	switch (mapNumber)
 	{
 	case 0:
-		for (int hChip = 0; hChip < MapDataFile::kChipNumY; hChip++)
-		{
-			for (int wChip = 0; wChip < MapDataFile::kChipNumX; wChip++)
-			{
-				// １は当たり判定チップを表しているので１のところだけ描画
-				if (PrototypeChipData[hChip][wChip].chipKind == 1)
-				{
-					auto leftTop = static_cast<int>(PrototypeChipData[hChip][wChip].pos.x - PrototypeChipData[hChip][wChip].w * 0.5f);
-					auto leftBottom = static_cast<int>(PrototypeChipData[hChip][wChip].pos.y - PrototypeChipData[hChip][wChip].h * 0.5f);
-					auto rightTop = static_cast<int>(PrototypeChipData[hChip][wChip].pos.x + PrototypeChipData[hChip][wChip].w * 0.5f);
-					auto rightBottom = static_cast<int>(PrototypeChipData[hChip][wChip].pos.y + PrototypeChipData[hChip][wChip].h * 0.5f);
-
-					DrawBox(
-						leftTop + static_cast<int>(camera->GetCameraDrawOffset().x),
-						leftBottom + static_cast<int>(camera->GetCameraDrawOffset().y),
-						rightTop + static_cast<int>(camera->GetCameraDrawOffset().x),
-						rightBottom + static_cast<int>(camera->GetCameraDrawOffset().y),
-						0xaaaaaa/*GetColor(255, 255, 255)*/, TRUE);
-				}
-
-			}
-		}
+		MapChipNumY = MapDataFile::kChipNumY;
+		MapChipNumX = MapDataFile::kChipNumX;
 		break;
 	case 1:
-		for (int hChip = 0; hChip < MapDataFile::kChipNumY; hChip++)
-		{
-			for (int wChip = 0; wChip < MapDataFile::kChipNumX; wChip++)
-			{
-				// １は当たり判定チップを表しているので99のところだけ描画
-				if (PrototypeChipData[hChip][wChip].chipKind == 99)
-				{
-					auto leftTop = static_cast<int>(PrototypeChipData[hChip][wChip].pos.x - PrototypeChipData[hChip][wChip].w * 0.5f);
-					auto leftBottom = static_cast<int>(PrototypeChipData[hChip][wChip].pos.y - PrototypeChipData[hChip][wChip].h * 0.5f);
-					auto rightTop = static_cast<int>(PrototypeChipData[hChip][wChip].pos.x + PrototypeChipData[hChip][wChip].w * 0.5f);
-					auto rightBottom = static_cast<int>(PrototypeChipData[hChip][wChip].pos.y + PrototypeChipData[hChip][wChip].h * 0.5f);
-
-					DrawBox(
-						leftTop + static_cast<int>(camera->GetCameraDrawOffset().x),
-						leftBottom + static_cast<int>(camera->GetCameraDrawOffset().y),
-						rightTop + static_cast<int>(camera->GetCameraDrawOffset().x),
-						rightBottom + static_cast<int>(camera->GetCameraDrawOffset().y),
-						0xaaaaaa/*GetColor(255, 255, 255)*/, TRUE);
-				}
-
-			}
-		}
+		MapChipNumY = MapDataFile::k1ChipNumY;
+		MapChipNumX = MapDataFile::k1ChipNumX;
 		break;
 	case 2:
-		break;
+	default:
+		assert(false);
+		return;
+	}
+	for (int hChip = 0; hChip < MapChipNumY; hChip++)
+	{
+		for (int wChip = 0; wChip < MapChipNumX; wChip++)
+		{
+			// １は当たり判定チップを表しているので１のところだけ描画
+			if (PrototypeChipData[hChip][wChip].chipKind == 1)
+			{
+				auto leftTop = static_cast<int>(PrototypeChipData[hChip][wChip].pos.x - PrototypeChipData[hChip][wChip].w * 0.5f);
+				auto leftBottom = static_cast<int>(PrototypeChipData[hChip][wChip].pos.y - PrototypeChipData[hChip][wChip].h * 0.5f);
+				auto rightTop = static_cast<int>(PrototypeChipData[hChip][wChip].pos.x + PrototypeChipData[hChip][wChip].w * 0.5f);
+				auto rightBottom = static_cast<int>(PrototypeChipData[hChip][wChip].pos.y + PrototypeChipData[hChip][wChip].h * 0.5f);
+
+				DrawBox(
+					leftTop + static_cast<int>(camera->GetCameraDrawOffset().x),
+					leftBottom + static_cast<int>(camera->GetCameraDrawOffset().y),
+					rightTop + static_cast<int>(camera->GetCameraDrawOffset().x),
+					rightBottom + static_cast<int>(camera->GetCameraDrawOffset().y),
+					0xaaaaaa/*GetColor(255, 255, 255)*/, TRUE);
+			}
+
+		}
 	}
 }
 /*for (int hChip = 0; hChip < MapDataFile::kChipNumY; hChip++)
